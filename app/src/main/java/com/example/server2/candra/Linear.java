e5a891de9b633f05f50c48b4013044fea25a48dd
Candra Herdiansyah <candraherdiansyah14@gmail.com>
2018-04-17 16:18:22 +0700

    Fix

---
+++ app/src/main/java/com/example/server2/candra/Linear.java
@@ -0,0 +1,13 @@
+package com.example.server2.candra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Linear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
